fd36ab4aba277873b18bef0c47a76b7d19e58ad6
JonathanCarvalho039 <jonathan.carvalho@sptech.school>
2024-06-20 10:45:54 -0300

    Adicionando primeira rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: NavComponent
+  },
+  {
+    path: '',
+    component: AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
